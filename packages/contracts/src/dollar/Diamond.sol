6ac9e87835770235c78b760da3e2005995f4ad2e
korrrba <88761781+gitcoindev@users.noreply.github.com>
2023-08-21 14:54:21 +0200

    Update Diamond.sol

--- packages/contracts/src/dollar/Diamond.sol
+++ packages/contracts/src/dollar/Diamond.sol
@@ -19,7 +19,7 @@ struct DiamondArgs {
 
 /**
  * @notice Contract that implements diamond proxy pattern
- * @dev Main protocol's entrypoint
+ * @dev Main protocol's entrypoint 1
  */
 contract Diamond {
     /**
