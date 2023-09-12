00416ffb6b63ed26a948d36b649eb0208a7e1143
korrrba <88761781+gitcoindev@users.noreply.github.com>
2023-09-12 22:01:14 +0200

    Update StakingShare.sol
    Move new variable to end of the struct

--- packages/contracts/src/dollar/core/StakingShare.sol
+++ packages/contracts/src/dollar/core/StakingShare.sol
@@ -18,13 +18,14 @@ contract StakingShare is ERC1155Ubiquity, ERC1155URIStorage {
         address minter;
         // lp amount deposited by the user
         uint256 lpFirstDeposited;
-        uint256 creationBlock;
-        uint256 findMe;
+        uint256 creationBlock;        
         // lp that were already there when created
         uint256 lpRewardDebt;
         uint256 endBlock;
         // lp remaining for a user
         uint256 lpAmount;
+        // move new variable to end of the struct
+        uint256 findMe;
     }
 
     /// @notice Mapping of stake id to stake info
