ae3d7ccd53810029ec4bb605197136576f73e615
korrrba <88761781+gitcoindev@users.noreply.github.com>
2023-09-16 19:55:00 +0200

    Update Diamond.sol

--- packages/contracts/src/dollar/Diamond.sol
+++ packages/contracts/src/dollar/Diamond.sol
@@ -10,7 +10,7 @@ import {DiamondCutFacet} from "./facets/DiamondCutFacet.sol";
 import {DiamondLoupeFacet} from "./facets/DiamondLoupeFacet.sol";
 import {OwnershipFacet} from "./facets/OwnershipFacet.sol";
 
-/// @notice Struct used for `Diamond` constructor args 1
+/// @notice Struct used for `Diamond` constructor args 2
 struct DiamondArgs {
     address owner;
     address init;
